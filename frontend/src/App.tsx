a27ac80d0902ad288104a16e6f969d4b1f1615c9
FreddyP <fredwardpach@gmail.com>
2025-03-10 11:33:49 -0400

    Set up Login and Register pages
    Login has parts of api connection.
    Register does not

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage';
-//import LoginPage from './pages/LoginPage';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
+import CarPage from './pages/CarPage';
+import TeamPage from './pages/Team';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path ="/carpage" element={<CarPage />} />
+          <Route path ="/team" element={<TeamPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/SidePicture/SidePicture.tsx
@@ -0,0 +1,9 @@
+import nightRoad from '../Images/NightRoad.png';
+import "./SidePicture.css";
+
+function SidePicture() {
+    
+    return <img src={nightRoad} alt="NightRoad" className="side-picture" />;
+}
+
+export default SidePicture;
