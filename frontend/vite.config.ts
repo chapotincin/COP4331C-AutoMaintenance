a635fe01516de917f2cfac4ec520e999e855e1be
Jayden Botha <jaybotha05@gmail.com>
2025-03-11 17:39:58 -0400

    updated vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [react(), tsconfigPaths()],
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
