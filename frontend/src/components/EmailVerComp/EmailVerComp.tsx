a2fcfbd0d62dc57d4965a70c494976af153924dd
FreddyP <fredwardpach@gmail.com>
2025-03-13 10:09:25 -0400

    API troubleshoot

--- frontend/src/components/EmailVerComp/EmailVerComp.tsx
+++ frontend/src/components/EmailVerComp/EmailVerComp.tsx
@@ -28,7 +28,7 @@ function EmailVerComp()
         }
         try
         {
-            const response = await fetch('http://159.203.135.123:5000/api/login',
+            const response = await fetch('http://localhost:5000/api/login',
                 {method:'POST',headers:{'Content-Type' :
                     'application/json'},body: JSON.stringify({
                         email,
