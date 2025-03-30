9ece8ba6efbd3c378576df5bc985342c1c647008
FreddyP <fredwardpach@gmail.com>
2025-03-30 17:06:03 -0400

    Set up car page
    Test 1, not formatted

--- frontend/src/components/HomeHeader/HomeHeader.tsx
+++ frontend/src/components/HomeHeader/HomeHeader.tsx
@@ -1,17 +1,21 @@
 import "./HomeHeader.css"
 import Logo from '../Logo/Logo'
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function HomeHeader() {
     const navigate = useNavigate();
-
+    const location = useLocation(); // Hook to get the current route
+    const currentPath = location.pathname;
+    const hideButtonOnPath = "/carpage"; // Example path
     return (
         <div id="header">
             <Logo />
             <div className="buttons-container">
                 <button id="homeButton" className="buttons" onClick={() => navigate("/")}>Home</button>
                 <button id="teamButton" className="buttons" onClick={() => navigate("/team")}>Team</button>
-                <button id="loginButton" className="buttons" onClick={() => navigate("/login")}>Log In</button>
+                {currentPath !== hideButtonOnPath && (
+                    <button id="loginButton" className="buttons" onClick={() => navigate("/login")}>Log In</button>
+                )}
             </div>
         </div>
     );
